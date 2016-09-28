d7183c3769dd42f382c0c74c2723ad0dd7153f79
pablogadhi <pablogadhi@gmail.com>
2016-09-27 21:53:39 -0600

    Prueba Interfaz

--- interfaz.java
+++ interfaz.java
@@ -1,15 +1,15 @@
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
-public class Interfaz1 {
+public class Interfaz {
 
 	private JFrame frame;
 
 	/**
 	 * Create the application.
 	 */
-	public Interfaz1() {
+	public Interfaz() {
 		initialize();
 		frame.setVisible(true);
 	}
@@ -19,8 +19,17 @@ public class Interfaz1 {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 645, 428);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 183, 227);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JButton btnPrueba = new JButton("Prueba");
+		btnPrueba.setBounds(58, 5, 67, 183);
+		panel.add(btnPrueba);
 	}
-
 }
